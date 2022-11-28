81eb2aeee5adc65e3c959dc1684b2a653180e19f
aritra_ghosh <aritra_ghosh@mkcinsey.com>
2022-11-28 11:54:16 +0530

    chore: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
@@ -9,6 +10,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser(process.env.JWT_SECRET));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  const config = new DocumentBuilder()
+    .setTitle('Accounts Microservice')
+    .setDescription('The Accounts API description')
+    .setVersion('1.0')
+    .addTag('accounts')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
